4e0a95782ef6f95961aaaa99dd4b03b73de55acd
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-09-21 08:19:01 +0200

    try20

--- src/server.js
+++ src/server.js
@@ -35,6 +35,16 @@ const corsOptions = {
 export function setupServer() {
   app.use(cors(corsConfigs));
 
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://event-board-ten.vercel.app',
+    );
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
   app.use(
     pino({
       transport: {
